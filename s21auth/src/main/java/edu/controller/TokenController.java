b9fb0b7a71fa13d7b47f7b370a3c9814b00130a2
Michael <muxaujish@gmail.com>
2025-03-09 01:33:57 +0300

    fix auth service

--- s21auth/src/main/java/edu/controller/TokenController.java
+++ s21auth/src/main/java/edu/controller/TokenController.java
@@ -1,14 +1,12 @@
 package edu.controller;
 
-import edu.model.TokenEntity;
 import edu.dto.TokenRequest;
 import edu.service.TokenService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -22,43 +20,29 @@ public class TokenController {
     }
 
     @PostMapping
-    public ResponseEntity<String> generateToken(@RequestBody TokenRequest request) {
-        try {
-            String accessToken = tokenService.getAccessToken(request.getLogin(), request.getPassword());
-            if (accessToken == null) {
-                log.error("Не удалось сгенерировать токен для {}", request.getLogin());
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-            return ResponseEntity.ok(accessToken);
-        } catch (Exception e) {
-            log.error("Ошибка генерации токена для {}: {}", request.getLogin(), e.getMessage());
+    public ResponseEntity<String> generateToken(@Valid @RequestBody TokenRequest request) {
+        String accessToken = tokenService.getAccessToken(request.getLogin(), request.getPassword());
+        if (accessToken == null) {
+            log.error("Не удалось сгенерировать токен для {}", request.getLogin());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+        return ResponseEntity.ok(accessToken);
     }
 
     @GetMapping("/default")
     public ResponseEntity<String> getDefaultTokenController() {
-        try {
-            String accessToken = tokenService.getDefaultAccessToken();
-            if (accessToken == null) {
-                log.error("Не удалось сгенерировать токен");
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-            return ResponseEntity.ok(accessToken);
-        } catch (Exception e) {
-            log.error("Ошибка генерации токена для: {}", e.getMessage());
+        String accessToken = tokenService.getDefaultAccessToken();
+        if (accessToken == null) {
+            log.error("Не удалось сгенерировать токен по умолчанию");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+        return ResponseEntity.ok(accessToken);
     }
 
     @GetMapping
     public ResponseEntity<String> getToken(@RequestParam String login) {
-        try {
-            Optional<TokenEntity> tokenEntityOpt  = tokenService.findByLogin(login);
-            return tokenEntityOpt.map(tokenEntity -> ResponseEntity.ok(tokenEntity.getAccessToken())).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-        } catch (Exception e) {
-            log.error("Ошибка получения токена для {}: {}", login, e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return tokenService.findByLogin(login)
+                .map(tokenEntity -> ResponseEntity.ok(tokenEntity.getAccessToken()))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
