f2cacb197bdf3bdafe5224829b4413c2f5bd5545
Michael <muxaujish@gmail.com>
2025-03-08 21:17:30 +0300

    fix sonar recomendations

--- s21edu_api/src/test/java/ru/school21/edu/service/TokenServiceTest.java
+++ s21edu_api/src/test/java/ru/school21/edu/service/TokenServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
