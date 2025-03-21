48834bf0e108eec1a2c4bba7b45ce67c1537511a
Michael <muxaujish@gmail.com>
2025-03-21 23:28:55 +0300

    added new module s21meet for telegram

---
+++ s21meet/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.11.0"
+}
+
+
+val springBootVersion: String by project
+val junitJupiterVersion: String by project
+val testcontainersVersion: String by project
+val resilience4jVersion: String by project
+val mockitoVersion: String by project
+val mapstructVersion: String by project
+val openApiVersion: String by project
+val gsonVersion: String by project
+val jacksonDatabind: String by project
+val squareupOkhttpVersion: String by project
+val gsonfireVersion: String by project
+val springRetryVersion: String by project
+val apacheCommonsVersion: String by project
+val springDotEnvVersion: String by project
+val wireMockVersion: String by project
+val jettyServerVersion: String by project
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-logging:$springBootVersion")
+    implementation("org.openapitools:openapi-generator-gradle-plugin:$openApiVersion")
+
+    implementation("org.telegram:telegrambots:6.9.7.1")
+
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    testImplementation("org.apache.commons:commons-compress:$apacheCommonsVersion")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+configurations.all {
+    resolutionStrategy {
+        // Фиксируем версию, чтобы не подтягивалась старая версия junit-platform-commons
+        force("org.junit.platform:junit-platform-commons:1.11.4")
+    }
+}
+
+
+springBoot {
+    mainClass.set("ru.school21.meet.Application")
+}
---
+++ s21meet/src/main/java/ru/school21/meet/S21meetApplication.java
@@ -0,0 +1,13 @@
+package ru.school21.meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S21meetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S21meetApplication.class, args);
+	}
+
+}
