7213c9c6633fa0fc5cd6171ae0cecffdb40a0e8f
Michael <muxaujish@gmail.com>
2025-05-17 22:01:08 +0300

    Получение сообщений только в приватном чате с ботом. Мелкие исправления

--- s21meet/src/main/java/ru/school21/meet/bot/SimpleBot.java
+++ s21meet/src/main/java/ru/school21/meet/bot/SimpleBot.java
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.chat.Chat;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -19,16 +20,19 @@ public class SimpleBot implements LongPollingSingleThreadUpdateConsumer {
     public void consume(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             Message message = update.getMessage();
+            Chat chat = message.getChat();
+            if (chat.getType().equals("private")) {
+                log.info("{} написал {}", chat.getUserName(), message.getText());
+                SendMessage response = SendMessage.builder()
+                        .chatId(message.getChatId())
+                        .text("Вы написали: " + message.getText())
+                        .build();
 
-            SendMessage response = SendMessage.builder()
-                    .chatId(message.getChatId())
-                    .text("Вы написали: " + message.getText())
-                    .build();
-
-            try {
-                telegramClient.execute(response);
-            } catch (TelegramApiException e) {
-                log.error("Ошибка отправки сообщения", e);
+                try {
+                    telegramClient.execute(response);
+                } catch (TelegramApiException e) {
+                    log.error("Ошибка отправки сообщения", e);
+                }
             }
         }
     }
