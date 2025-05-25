f51590817618909ebfe7519804e64120520b1345
Michael <muxaujish@gmail.com>
2025-05-25 12:17:44 +0300

    fix gradle verification

--- s21auth/src/test/java/ru/izpz/auth/config/TokenControllerConfig.java
+++ s21auth/src/test/java/ru/izpz/auth/config/TokenControllerConfig.java
@@ -8,20 +8,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
-
 @ActiveProfiles("test")
 @TestConfiguration
 public class TokenControllerConfig {
 
     @Bean
     public TokenService tokenService() {
-        // Задаём фиктивные значения для конструктора
         String defaultLogin = "dummyLogin";
         String defaultPassword = "dummyPassword";
-        // Мокаем зависимости
+
         TokenRepository tokenRepository = Mockito.mock(TokenRepository.class);
         RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
-        // Возвращаем spy, чтобы можно было переопределить отдельные методы
+
         return Mockito.spy(new TokenService(defaultLogin, defaultPassword, tokenRepository, restTemplate));
     }
 }
--- s21edu/src/test/java/ru/izpz/edu/service/TokenServiceTest.java
+++ s21edu/src/test/java/ru/izpz/edu/service/TokenServiceTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 import ru.izpz.auth.exception.TokenResponseException;
-import ru.izpz.edu.service.TokenService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
