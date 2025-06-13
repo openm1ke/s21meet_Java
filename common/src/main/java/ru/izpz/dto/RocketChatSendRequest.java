01869d656395783bf55a9bcd46a3b8cd9a029493
Michael <muxaujish@gmail.com>
2025-06-13 19:32:45 +0300

    s21rocket module api client

---
+++ common/src/main/java/ru/izpz/dto/RocketChatSendRequest.java
@@ -0,0 +1,13 @@
+package ru.izpz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RocketChatSendRequest {
+    private String username;
+    private String message;
+}
