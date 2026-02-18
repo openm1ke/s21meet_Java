2d70e29e44ac18b34488839cd479a15fa48429f1
openmike <muxaujish@gmail.com>
2026-02-18 20:19:00 +0300

    s21bot telegramclientproxy adapter improved

--- s21bot/src/main/java/ru/izpz/bot/service/TelegramClientProxy.java
+++ s21bot/src/main/java/ru/izpz/bot/service/TelegramClientProxy.java
@@ -7,10 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.Serializable;
@@ -21,29 +17,6 @@ import java.io.Serializable;
 public class TelegramClientProxy {
 
     private final OkHttpTelegramClient delegate;
-
-    @RateLimiter(name = "telegram")
-    @Retry(name = "telegram")
-    public void sendMessage(Long chatId, String text, ReplyKeyboard keyboard) throws TelegramApiException {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .text(text)
-                .replyMarkup(keyboard)
-                .build();
-        delegate.execute(msg);
-    }
-
-    @RateLimiter(name = "telegram")
-    @Retry(name = "telegram")
-    public void editMessage(Long chatId, Integer messageId, String text, InlineKeyboardMarkup kb) throws TelegramApiException {
-        var edit = EditMessageText.builder()
-                .chatId(chatId.toString())
-                .messageId(messageId)
-                .text(text)
-                .replyMarkup(kb)
-                .build();
-        delegate.execute(edit);
-    }
 
     @RateLimiter(name = "telegram")
     @Retry(name = "telegram")
