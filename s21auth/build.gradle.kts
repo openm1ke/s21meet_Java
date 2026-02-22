702d495df48b386e41fd512b1ffae80174377761
openmike <muxaujish@gmail.com>
2026-02-23 00:36:51 +0300

    adds prometheus and grafana

--- s21auth/build.gradle.kts
+++ s21auth/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("me.paulschwarz:springboot3-dotenv:${springDotEnvVersion}")
     implementation("io.github.resilience4j:resilience4j-spring-boot3:${resilience4jVersion}")
     implementation("io.github.resilience4j:resilience4j-ratelimiter:${resilience4jVersion}")
